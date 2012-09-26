ad98cf390415a14987e0ce49a9d23bbb82c2f342
hitoyozake <mikoto_1122@yahoo.co.jp>
2012-09-27 03:14:28 +0900

    VBのほうも書いた

--- MultiKinectProject/RecorderGUI/Form1.Designer.vb
+++ MultiKinectProject/RecorderGUI/Form1.Designer.vb
@@ -22,17 +22,65 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 199)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(300, 281)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "記録開始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "測定時のメモ"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 40)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(300, 153)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(160, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(154, 21)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "オプション"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(440, 492)
+        Me.ClientSize = New System.Drawing.Size(326, 492)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "KinectRecorder"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- MultiKinectProject/RecorderGUI/Form1.vb
+++ MultiKinectProject/RecorderGUI/Form1.vb
@@ -1,3 +1,32 @@
 ﻿Public Class Form1
 
+    '録画ソフト起動用プロセス
+    Public process As Process = Nothing
+    Public standardInput As String
+    Public standardOutput As String
+
+    Private Sub InitInstance()
+
+        process = New Process()
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.FileName = "hoge.exe"
+
+
+    End Sub
+
+
+    Private Sub NewInstance()
+
+        If process Is Nothing Then
+        End If
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        NewInstance()
+
+    End Sub
 End Class
