157e546bf21e40ef8ee82f89dd38b9a781ef9d82
hitoyozake <mikoto_1122@yahoo.co.jp>
2013-02-10 22:16:41 +0900

    GUIの中身の処理をおおまかに作成した. filesystem.cppにファイル容量を返す処理を作成した. データ保存時にこれをチェックしてディスク溢れを防ぐことができるはず.

--- KinectTest/NewRecordingSoft/Form1.Designer.cs
+++ KinectTest/NewRecordingSoft/Form1.Designer.cs
@@ -28,19 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(260, 237);
             this.button1.TabIndex = 0;
             this.button1.Text = "START RECORDING";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -50,6 +56,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +64,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- KinectTest/NewRecordingSoft/Form1.cs
+++ KinectTest/NewRecordingSoft/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,27 +13,116 @@ namespace NewRecordingSoft
 {
     public partial class Form1 : Form
     {
-        public Boolean started_ = false;
+        public Boolean started = false;
+        public System.IO.StreamWriter debugLog = null;
+
+        public Process process = null;
+        public int timerCounter = 0;
+
+        public String filename = "recorder.exe";
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void InitProcess()
+        {
+            process = new Process();
+            process.StartInfo.FileName = filename;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardOutput = false;
+        }
+
+        private void CloseProcess()
+        {
+            if( process != null )
+            {
+                process.StandardInput.WriteLine( "end" );
+                //タイマーでExitを待ち続ける
+                timer1.Enabled = true;
+            }
+        }
+
+        private String GenerateFileName()
+        {
+            var year = System.DateTime.Now.Year.ToString( "0000" );
+            var month = System.DateTime.Now.Month.ToString("00");
+            var day = System.DateTime.Now.Day.ToString("00");
+            var hour = System.DateTime.Now.Hour.ToString("00");
+            var minute = System.DateTime.Now.Minute.ToString("00");
+
+            return year + "_" + month + "_" + day + "_" + hour + "_" + minute;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            if (started_)
+            if (started)
             {
                 button1.Text = "START RECORDING";
-                started_ = false;
+                started = false;
+                button1.Enabled = false;
+
+                CloseProcess();
             }
             else
             {
-                button1.Text = "STOP RECORDING";
-                button1.Enabled = false;
-                started_ = true;
+                if (System.IO.File.Exists(filename))
+                {
+                    button1.Enabled = false;
+                    button1.Text = "STOP RECORDING";
 
-                //撮影開始まで待機
+                    InitProcess();
+                    process.Start();
+
+                    started = true;
+
+                    button1.Enabled = true;
+                    //撮影開始まで待機
+                }
+                else
+                {
+                    MessageBox.Show("計測ソフトが存在しません(recorder.exe does not exist");
+                    debugLog.WriteLine("REDORDER.EXE DOES NOT EXIST");
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            debugLog = new System.IO.StreamWriter("debugLog.txt");
+
+            //現在時刻を書き込む
+            debugLog.WriteLine(DateTime.Now);
+            debugLog.WriteLine("******************************");
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (process != null)
+            {
+                if (process.HasExited)
+                {
+                    started = false;
+                    button1.Enabled = true;
+                    timerCounter = 0;
+                }
+                else if (timerCounter > 1000)
+                {
+                    //何らかの理由で終了できない
+                    process.Kill(); //本当はよろしくない
+                }
+                ++timerCounter;
+            }
+            else
+            {
+                started = false;
+                button1.Enabled = true;
+                timerCounter = 0;
             }
         }
     }
