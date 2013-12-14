a9979b0cf2a206137a32be7abfefaf292e17f946
hitoyozake <mikoto_1122@yahoo.co.jp>
2013-12-14 11:47:26 +0900

    add

---
+++ BoneEstimation/BoneEstimation/BoneEstimation/PoseEvaluater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace BoneEstimation
+{
+
+
+    class PoseEvaluater
+    {
+
+        Color[] pixels = new Color[ 640 * 480 ];
+
+
+        float evaluate()
+        {
+            return 0f;
+        }
+
+    }
+}
